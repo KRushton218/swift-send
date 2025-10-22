024959344ba8ca1687bc377db65a5f1a36c82e21
KRushton218 <44506798+KRushton218@users.noreply.github.com>
2025-10-22 16:47:07 -0400

    Initial commit

---
+++ swift-send/swift_sendApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_sendApp.swift
+//  swift-send
+//
+//  Created by Kiran Rushton on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_sendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-sendTests/swift_sendTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_sendTests.swift
+//  swift-sendTests
+//
+//  Created by Kiran Rushton on 10/22/25.
+//
+
+import Testing
+@testable import swift_send
+
+struct swift_sendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
